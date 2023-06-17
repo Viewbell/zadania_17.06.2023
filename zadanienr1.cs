40a7cae9587a2c133f4e2bd5ff5eb722825c576f
Viewbell <128227140+Viewbell@users.noreply.github.com>
2023-06-17 11:05:31 +0200

    Add files via upload

---
+++ zadanienr1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+
+        public static void Main()
+        {
+            int a, b, wynik = 1;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            while (b > 0)
+            {
+                wynik *= a;
+                b--;
+            }
+
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}

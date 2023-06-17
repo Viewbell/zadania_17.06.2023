a399a996d5d0bd680f99dcc6779b51a71f15e189
Viewbell <128227140+Viewbell@users.noreply.github.com>
2023-06-17 10:19:47 +0200

    Add files via upload

---
+++ zadanie nr 5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static double obliczanieSilni(int liczba)
+        {
+            double wynik = 1;
+            for (int i = 2; i <= liczba; i++)
+            {
+                wynik *= i;
+
+            }
+            return wynik;
+        }
+        static double funkcjaWykladnicza(double x, int n)
+        {
+            double wynik = 0;
+            for (int i = 00; i <= n; i++)
+            {
+                double warunek = Math.Pow(x, i) / obliczanieSilni(i);
+            }
+            return wynik;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość x: ");
+            double x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj Liczbe wyrazów n:");
+            int n = int.Parse(Console.ReadLine());
+            double wartoscWykladnicza = funkcjaWykladnicza(x, n);
+            Console.WriteLine($"Wartość funkcji e^{x} dla n = {n} wynosi: {wartoscWykladnicza}");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
